29d49df33045f08b42113c6a7f3e25dfbc002f13
2025050190 <bria051@kau.kr>
2026-04-29 16:15:02 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    int money = 0, A = 0, B = 0, C = 0, flag = 0;
+    int money = 0, A = 0, B = 0, C = 0, exist = false;
     scanf("%d", &money);
 
-    int x = money / 900, y = 0, z = 0;
-    for (int i = 0; i <= x; i++) {
-        if (money - 900 * i > 0)
-            A ++;
-
-        y = (money - 900 * A) / (750 * 2);
+    for (int i = money - 900; i >= 0; i -= 900) {
+        A ++;
         B = 0;
-        for (int j = 0; j <= y; j++) {
-
-            if (money - 750 * 2 * j > 0)
-                B += 2;
-
-            z = money - 900 * A - 750 * B;
-
-            C = z / 200;
-            if (z % 200 == 0 && C > 0 && (C < A || C < B)) {
-                printf("%d %d %d\n", A, B, C);
-                flag = 1;
+        for (int j = i - 1500; j >= 0; j -= 750 * 2) {
+            B += 2;
+            C = 0;
+            for (int k = j - 200; k >= 0; k -= 200) {
+                C += 1;
+                if (k == 0 && (C < A || C < B)) {
+                    printf("%d %d %d\n", A, B, C);
+                    exist = true;
+                }
             }
         }
     }
 
-    if (flag == 0)
+    if (!exist)
         printf("none");
 
     return 0;
