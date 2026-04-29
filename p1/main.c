4e790af3035bdf0b0ca1d782a77f742d0f8752e9
2025050190 <bria051@kau.kr>
2026-04-29 16:39:34 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdbool.h> // 엣날 버전이라 적었습니다.
 
 int main()
 {
-    int money = 0, A = 0, B = 0, C = 0, exist = false;
+
+    int money = 0;
+    bool exist = false;
     scanf("%d", &money);
 
-    for (int i = money - 900; i >= 0; i -= 900) {
-        A ++;
-        B = 0;
-        for (int j = i - 1500; j >= 0; j -= 750 * 2) {
-            B += 2;
-            C = 0;
-            for (int k = j - 200; k >= 0; k -= 200) {
-                C += 1;
-                if (k == 0 && (C < A || C < B)) {
-                    printf("%d %d %d\n", A, B, C);
+    int sum = 0;
+    for (int x = 1; x < money / 900; x++) {
+        for (int y = 2; y < money / 750; y+=2) {
+            for (int z = 1; z < money / 200; z++) {
+                sum = 900 * x + 750 * y + 200 * z;
+                if (sum == money && (z < x || z < y)) {
+                    printf("%d %d %d\n", x, y, z);
                     exist = true;
                 }
             }
